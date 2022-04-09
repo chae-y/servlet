5e3da0314bcdee7d753f48b48715d5b9edb2a7e0
chae <stat_chae0@sookmyung.ac.kr>
2022-04-09 15:49:07 +0900

    HttpServletRequest - 기본사용법

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name="requestHeaderServlet", urlPatterns = "/request-header")
-public class RequestHeaderSerlet extends HttpServlet {
+public class RequestHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
