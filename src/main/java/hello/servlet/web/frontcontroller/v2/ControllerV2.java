a2077f1ed72bb43398e88bf864fdf94b5708f706
chae <stat_chae0@sookmyung.ac.kr>
2022-04-16 16:33:21 +0900

    View 분리 - v2 수정

--- src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -1,4 +1,4 @@
-package hello.servlet.web.frontcontroller.v2.controller;
+package hello.servlet.web.frontcontroller.v2;
 
 import hello.servlet.web.frontcontroller.MyView;
 
--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -3,14 +3,14 @@ package hello.servlet.web.frontcontroller.v2.controller;
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MemberSaveControllerV2 implements ControllerV2{
+public class MemberSaveControllerV2 implements ControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
